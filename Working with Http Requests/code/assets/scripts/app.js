14062c27b5b3ce340264c93ad3a5d65a5c4bc859
Ascend Ho <coderhs@foxmail.com>
2023-03-06 11:21:02 +0800

    error handling

--- Working with Http Requests/code/assets/scripts/app.js
+++ Working with Http Requests/code/assets/scripts/app.js
@@ -2,27 +2,84 @@
 
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
+function sendHttpRequest(method, url, data) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.responseType = 'json';
 
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+        xhr.onload = function () {
+            if (xhr.status >= 200 && xhr.status < 300)
+                resolve(xhr.response);
+            else
+                reject(new Error('Something went wrong!'));
+            // const listOfPosts = JSON.parse(xhr.response);
+        }
 
-xhr.responseType = 'json';
-xhr.send();
+        xhr.onerror = function () {
+            reject(new Error('Failed to send request!'));
+        }
 
-xhr.onload = function () {
-    // const listOfPosts = JSON.parse(xhr.response);
-    const listOfPosts = xhr.response;
-    for (const post of listOfPosts) {
-        const postEl = document.importNode(postTemplate.content,true);
-        postEl.querySelector('h2').textContent = post.title.toUpperCase();
-        postEl.querySelector('p').textContent = post.body;
-        listElement.append(postEl);
+        xhr.send(JSON.stringify(data));
+
+    });
+}
+
+async function fetchPosts() {
+    try {
+        const listOfPosts = await sendHttpRequest('GET',
+            'https://jsonplaceholder.typicode.com/posts');
+        for (const post of listOfPosts) {
+            const postEl = document.importNode(postTemplate.content, true);
+            postEl.querySelector('h2').textContent = post.title.toUpperCase();
+            postEl.querySelector('p').textContent = post.body;
+            postEl.querySelector('li').id = post.id;
+            listElement.append(postEl);
+        }
+
+        console.log(listOfPosts);
+    } catch (error) {
+        alert(error.message);
     }
 
-    console.log(listOfPosts);
 
 }
 
+async function createPost(title, content, userId = Math.random()) {
+    const post = {
+        title: title,
+        body: content,
+        userId: userId
+    };
+    sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
+
+}
+
+
+fetchButton.addEventListener('click', fetchPosts);
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const enteredTitle = event.currentTarget.querySelector("#title").value;
+    const enteredContent = event.currentTarget.querySelector("#content").value;
+    createPost(enteredTitle, enteredContent);
+})
+
+let isClicked = false;
+postList.addEventListener('click', event => {
+    if (event.target.tagName === 'BUTTON') {
+        // console.log(event.target);
+        // console.log(event.target.closest('li'));
+        const postId = event.target.closest('li').id;
+        sendHttpRequest('DELETE', `https://jsonplaceholder.typicode.com/posts/${postId}`);
+        // console.log(postId);
+    }
+})
+
+// fetchPosts();
+// createPost('DUMMY', 'A dummy post!');
 
 
